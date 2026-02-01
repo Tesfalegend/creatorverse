d916385578f03f746b5e613f92f9cc464c5d7db9
Tesfatsion Desta <tesfatsion37@gmail.com>
2026-01-31 18:16:56 -0600

    Add walkthrough GIF

--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-const URL = 'https://niiklwitnagubostidqb.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5paWtsd2l0bmFndWJvc3RpZHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2MzY3NTAsImV4cCI6MjA4NTIxMjc1MH0.px68J-r9kLa7gH7vXGm62OviRU5dNVbFyZ6g8vnRO18';
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(URL, API_KEY);
